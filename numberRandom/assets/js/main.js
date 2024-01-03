04006e8e519d134116798871e09b473decb27304
Javan Luiz <144359052+DevJavanLuiz@users.noreply.github.com>
2024-01-03 16:55:21 -0300

    75% concluido

--- numberRandom/assets/js/main.js
+++ numberRandom/assets/js/main.js
@@ -5,32 +5,37 @@ resultNumberDrawn.appendChild(responseP);
 
 const userInput = document.querySelector('#user-response');
 
-
+let lastRandom;
 divResult.addEventListener('submit', function (event){
     event.preventDefault(); 
     
 });
 
 function numberRandom () {
-    const random = Math.random () * (50 - 1) + 1;
+    const random = Math.random () * (5 - 0) + 1;
+    const rand = Math.floor(random)
     responseP.style.backgroundColor = '#0076a3';
-    
-    var resNumberR = random.toFixed(0);
-    console.log(resNumberR);
-    
-    return responseP.innerHTML = random.toFixed(0);
+    responseP.style.color = '#0076a3';
+    responseP.innerHTML = rand;
+
+    lastRandom = rand
+    return  rand;
 }
 
 function numberSent (){
     const valueInput = Number(userInput.value);
-    const valueP = Number(resNumberR);
+    const valueP = lastRandom;
+    
     
     if (valueInput === valueP){
-        console.log('igual');
+        responseP.style.backgroundColor = 'green';
+        responseP.style.color = 'white';
     }
     else{ 
-        console.log('diferente');
+        responseP.style.backgroundColor = 'red';
+        responseP.style.color = 'red' 
     }
 
-    return console.log(valueInput, valueP);
+    console.log(valueP, valueInput);
+    return  valueP, valueInput;
 }
