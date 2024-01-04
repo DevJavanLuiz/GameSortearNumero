abbd5ed92d7dfb2ca4f5b84ea980bbf6374c4e9f
Javan Luiz <144359052+DevJavanLuiz@users.noreply.github.com>
2024-01-04 17:27:36 -0300

    80% concluido, ja feito a impressão dos resultados

--- numberRandom/assets/js/main.js
+++ numberRandom/assets/js/main.js
@@ -2,13 +2,12 @@ const divResult = document.querySelector('.result')
 const resultNumberDrawn = document.querySelector('#number-drawn');
 const responseP = document.createElement('p');
 resultNumberDrawn.appendChild(responseP);
-
+const h2 = document.querySelector('#result-input');
 const userInput = document.querySelector('#user-response');
 
 let lastRandom;
 divResult.addEventListener('submit', function (event){
     event.preventDefault(); 
-    
 });
 
 function numberRandom () {
@@ -17,7 +16,8 @@ function numberRandom () {
     responseP.style.backgroundColor = '#0076a3';
     responseP.style.color = '#0076a3';
     responseP.innerHTML = rand;
-
+    h2.innerHTML = 'Boa sorte!';
+    h2.style.color = '#0076a3'
     lastRandom = rand
     return  rand;
 }
@@ -28,12 +28,17 @@ function numberSent (){
     
     
     if (valueInput === valueP){
-        responseP.style.backgroundColor = 'green';
+        responseP.style.backgroundColor = 'rgba(58, 252, 58, 0.658)';
         responseP.style.color = 'white';
+        h2.innerHTML = 'Parabéns você achou!'
+        h2.style.color = 'rgba(58, 252, 58, 0.658)';
     }
     else{ 
-        responseP.style.backgroundColor = 'red';
-        responseP.style.color = 'red' 
+        responseP.style.backgroundColor = 'rgba(255, 0, 0, 0.9)';
+        responseP.style.color = 'rgba(255, 0, 0, 0.01)'; 
+        h2.innerHTML = 'Tente Novamente!'
+        h2.style.color = 'rgba(255, 0, 0, 0.9)'; 
+
     }
 
     console.log(valueP, valueInput);
